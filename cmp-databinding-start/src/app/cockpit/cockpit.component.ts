21e49745fafd04fca2bcd234dbf283772e390537
jrayson <55762149+jarr1357@users.noreply.github.com>
2022-05-15 14:10:00 -0400

    Local References with @ViewChild
    Modified serverContent to use local references with @ViewChild instead of two-way databinding.

--- cmp-databinding-start/src/app/cockpit/cockpit.component.ts
+++ cmp-databinding-start/src/app/cockpit/cockpit.component.ts
@@ -1,5 +1,5 @@
 import { HtmlParser } from '@angular/compiler';
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -12,7 +12,8 @@ export class CockpitComponent implements OnInit {
   @Output('bpCreated') blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
 
   //newServerName = '';
-  newServerContent = '';
+  // newServerContent = '';
+  @ViewChild('serverContentInput') serverContentInput: ElementRef;
 
   constructor() { }
 
@@ -20,10 +21,10 @@ export class CockpitComponent implements OnInit {
   }
 
   onAddServer(nameInput: HTMLInputElement) {
-    this.serverCreated.emit({serverName: nameInput.value, serverContent: this.newServerContent});
+    this.serverCreated.emit({serverName: nameInput.value, serverContent: this.serverContentInput.nativeElement.value});
   }
 
   onAddBlueprint(nameInput: HTMLInputElement) {
-    this.blueprintCreated.emit({serverName: nameInput.value, serverContent: this.newServerContent})
+    this.blueprintCreated.emit({serverName: nameInput.value, serverContent: this.serverContentInput.nativeElement.value})
   }
 }
